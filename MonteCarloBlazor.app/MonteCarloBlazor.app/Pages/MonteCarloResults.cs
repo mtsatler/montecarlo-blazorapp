4862cc86767bbd99dba826a85056fe8f4644e024
Mike Satler <mts5005@gmail.com>
2021-03-20 14:06:14 -0400

    seperate C# code from razor pages, moved into partial classes

---
+++ MonteCarloBlazor.app/MonteCarloBlazor.app/Pages/MonteCarloResults.cs
@@ -0,0 +1,54 @@
+﻿using MonteCarloConsole.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonteCarloBlazor.app.Pages
+{
+    public partial class MonteCarloResults
+    {
+
+        private int InitialValue { get; set; }
+
+        public int AnnualWithdraw { get; set; }
+
+        public int TimePeriod { get; set; }
+
+        public string Rebalance { get; set; }
+
+        public double AverageReturn { get; set; }
+
+        public double StdDeviation { get; set; }
+
+        private MonteCarlo MonteCarloSim = new MonteCarlo();
+
+        private void GetInputFromStateContainer()
+        {
+            InitialValue = StateContainer.InitialValue;
+            AnnualWithdraw = StateContainer.AnnualWithdraw;
+            TimePeriod = StateContainer.TimePeriod;
+            Rebalance = StateContainer.Rebalance;
+            AverageReturn = StateContainer.AverageReturn;
+            StdDeviation = StateContainer.StdDeviation;
+
+            MonteCarloSim = new MonteCarlo(InitialValue, AnnualWithdraw, TimePeriod, Rebalance,
+                           AverageReturn, StdDeviation);
+
+            //testString = MonteCarloSim.ReturnSample();
+        }
+
+        protected override void OnInitialized()
+
+        {
+            StateContainer.OnChange += StateHasChanged;
+
+        }
+
+        public void Dispose()
+        {
+            StateContainer.OnChange -= StateHasChanged;
+        }
+
+    }
+}
--- MonteCarloBlazor.app/MonteCarloBlazor.app/Pages/UserInput.cs
+++ MonteCarloBlazor.app/MonteCarloBlazor.app/Pages/UserInput.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,61 @@ namespace MonteCarloBlazor.app.Pages
 {
     public partial class UserInput
     {
-        //TODO move @code block to here
+
+        private int initialValue { get; set; }
+
+        private int annualWithdrawal { get; set; }
+
+        private double averageReturn { get; set; }
+
+        private double standardDev { get; set; }
+ 
+        private int yearsToSim { get; set; } = 10;
+
+        private string getWithdrawalAmount;
+
+        private string getPortfolioAmount;
+
+        private string getAverageReturn;
+
+        private string getStandardDev;
+
+        
+        private string rebalancingSelection { get; set; } = "None";
+
+        private void GetUserInput()
+        {
+            initialValue = int.Parse(getPortfolioAmount);
+            annualWithdrawal = int.Parse(getWithdrawalAmount);
+            averageReturn = double.Parse(getAverageReturn);
+            standardDev = double.Parse(getStandardDev);
+        }
+
+        private void SendUserInput()
+        {
+            GetUserInput();
+            StateContainer.SetInputReceived(true, initialValue, annualWithdrawal, yearsToSim, rebalancingSelection, averageReturn, standardDev);
+        }
+
+        private void GetRebalancingSelection(ChangeEventArgs args)
+        {
+            rebalancingSelection = args.Value.ToString();
+        }
+
+        private void GetYearsToSimSelection(ChangeEventArgs args)
+        {
+            yearsToSim = int.Parse(args.Value.ToString());
+        }
+
+        protected override void OnInitialized()
+        {
+            StateContainer.OnChange += StateHasChanged;
+        }
+
+        public void Dispose()
+        {
+            StateContainer.OnChange -= StateHasChanged;
+        }
 
     }
 }
